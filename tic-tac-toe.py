fa4cf9a7390cde00c6d6306593538eaa42cfcc5c
OlegKitGit <oleg2448@gmail.com>
2018-06-16 09:12:02 +0300

    Add files via upload

---
+++ tic-tac-toe.py
@@ -0,0 +1,89 @@
+#from termcolor import colored
+
+def draw_board(board):
+    print("\033[35m----------------------------------------------------------------------------")
+    for i in range(10):
+        
+        if i == 0:
+            print ("| ", board[0+i*10], "  | ", board[1+i*10], "  | ", board[2+i*10], " |", board[3+i*10], "  | ", board[4+i*10], "  | ", board[5+i*10], "  | ", board[6+i*10], "  | ", board[7+i*10], "  | ", board[8+i*10], "  | ", board[8+i*10], "  | ", board[9+i*10], " | ")
+        elif i == 9:
+            print ("| ", board[0+i*10], " | ", board[1+i*10], " | ", board[2+i*10], "|", board[3+i*10], " | ", board[4+i*10], " | ", board[5+i*10], " | ", board[6+i*10], " | ", board[7+i*10], " | ", board[8+i*10], " | ", board[8+i*10], " | ", board[9+i*10], "| ")
+        else:
+            print ("| ", board[0+i*10], " | ", board[1+i*10], " | ", board[2+i*10], "|", board[3+i*10], " | ", board[4+i*10], " | ", board[5+i*10], " | ", board[6+i*10], " | ", board[7+i*10], " | ", board[8+i*10], " | ", board[8+i*10], " | ", board[9+i*10], " | ")
+        print ("----------------------------------------------------------------------------")
+        
+
+
+
+def take_input(player_token):
+    global escape
+    valid = False
+    while not valid:
+        player_answer = input("\033[37mWhere to put " + player_token+"? ")
+        try:
+            player_answer = int(player_answer)
+        except:
+            if player_answer == 'exit':
+                escape = True
+                break
+            print("Invalid input. Are you sure that you entered a number?")
+            continue
+        if player_answer >= 1 and player_answer <= 100:
+            val = str(board[player_answer-1]).strip()
+            if (val != '\033[32mO\033[35m' and val != '\033[32mX\033[35m'):
+                if player_answer < 10:
+                    board[player_answer-1] = '\033[32m' + player_token +  '\033[35m'
+                else:
+                    board[player_answer-1] = '\033[32m' + player_token + '\033[35m' + ' '
+                valid = True
+            else:
+                print("This cell is already taken")
+        else:
+            print("Invalid input. Enter a number from 1 to 9.")
+
+
+def check_win(board):
+    a = board
+    for i in range(10):
+        for j in range(6):
+            if str(a[j+i*10]).strip() == str(a[j+1+i*10]).strip() == str(a[j+2+i*10]).strip() == str(a[j+3+i*10]).strip() == str(a[j+4+i*10]).strip():
+                return a[j+i*10]
+            elif str(a[i+j*10]).strip() == str(a[i+(j+1)*10]).strip() == str(a[i+(j+2)*10]).strip() == str(a[i+(j+3)*10]).strip() == str(a[i+(j+4)*10]).strip():
+                return a[i+j*10]
+            elif str(a[i+j*10]).strip() == str(a[i+1+(j+1)*10]).strip() == str(a[i+2+(j+2)*10]).strip() == str(a[i+3+(j+3)*10]).strip() == str(a[i+4+(j+4)*10]).strip():
+                return a[i+j*10]
+    for i in range(6):
+        for j in list(reversed(range(5, 10))):
+            if str(a[j+i*10]).strip() == str(a[(j-1)+(i+1)*10]).strip() == str(a[(j-2)+(i+2)*10]).strip() == str(a[(j-3)+(i+3)*10]).strip() == str(a[(j-4)+(4+i)*10]).strip():
+                return a[j+i*10]
+    return False
+
+def main(board):
+    global escape
+    counter = 0
+    win = False
+    escape = False
+    while not win:
+        draw_board(board)
+        if counter % 2 == 0:
+            take_input("X")
+        else:
+            take_input("O")
+        if escape == True:
+            break
+        counter += 1
+        if counter > 8:
+            tmp = check_win(board)
+            if tmp:
+                draw_board(board)
+                print(tmp, "Win!")
+                win = True
+                break
+        if counter == 100:
+            draw_board(board)
+            print("Draw!")
+            break
+    
+
+board = list(range(1,101))
+main(board)
